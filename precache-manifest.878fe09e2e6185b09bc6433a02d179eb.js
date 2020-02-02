2d735328fc88ef5920bc9873f9d141ef4fb520a7
arenchilingaryan <58465410+arenchilingaryan@users.noreply.github.com>
2020-02-02 11:54:06 +0300

    Updates

---
+++ precache-manifest.878fe09e2e6185b09bc6433a02d179eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97b2b37ea9df0b5a373caa9b1e2bb88",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "f5564b5abf656b7a6d8d",
+    "url": "/todo-list-react/static/css/main.7b285184.chunk.css"
+  },
+  {
+    "revision": "16610f4ebb5259eee449",
+    "url": "/todo-list-react/static/js/2.e573577f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo-list-react/static/js/2.e573577f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5564b5abf656b7a6d8d",
+    "url": "/todo-list-react/static/js/main.de1d1792.chunk.js"
+  },
+  {
+    "revision": "352883d3c8b82ce98d12",
+    "url": "/todo-list-react/static/js/runtime-main.39a6184b.js"
+  }
+]);
